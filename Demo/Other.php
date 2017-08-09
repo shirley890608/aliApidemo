05671f27be972b7d2174bb1f9102d9fdb78c7159
shirley890608 <admin@lulijuan.com>
2017-08-09 10:43:29 +0800

    test

---
+++ Demo/Other.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shirley
+ * Date: 2017/8/8
+ * Time: 上午11:54
+ */
+
+class Other
+{
+
+    public function getAliOtherApi($inputData)
+    {
+        //test
+    }
+
+    public function getMachineStudy()
+    {
+
+    }
+}
